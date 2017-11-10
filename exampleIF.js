bbf0eaf2cd58528e0fe4e336d8f6c184154aa150
bcarranza <bayron.carranza@gmail.com>
2017-11-10 00:07:50 -0600

    Se hace commit por primera vez

---
+++ exampleIF.js
@@ -0,0 +1,16 @@
+
+function Personal(leng) {
+    if (leng == "castellano")
+         alert("Hola Mundo");
+    else
+         if (leng == "ingles")
+            alert("Hello World");
+         else 
+              if (leng == "frances")
+                alert("Salut Monde");
+              else
+     if (leng == "alemán")
+                    alert("Hallo Welt");    
+                     else
+                     alert("Idioma no existe");
+}
